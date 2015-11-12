9997775d9a3eae062d168d70ef9b7e1ef2163a7d
kobabasu <keiji.kobayashi.web@gmail.com>
2015-11-13 00:49:09 +0900

    イラストを配置するメソッドを追加

--- src/pages/front/Temp.jsx
+++ src/pages/front/Temp.jsx
@@ -41,30 +41,42 @@ export default class Temp extends React.Component {
 
   init() {
     let id = this.props.params.id;
-    let el = document.getElementById('Palette');
     let canvas = document.createElement('canvas');
+    canvas.width = 620;
+    canvas.height = 877;
+    let ctx = canvas.getContext('2d');
+    
 
-    let item = this.state.canvas;
+    let item = this.state.px;
 
     let bg = new Image();
     bg.src = '../imgs/print_template0' + id + '.gif';
     
     let _this = this;
     bg.onload = function() {
-      let ctx = _this.attachTemp(canvas, bg);
-      el.appendChild(canvas);
+      ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
+      ctx = _this.placeItems(canvas, ctx);
     }
   }
 
-  attachTemp(canvas, bg) {
-    let ctx = canvas.getContext('2d');
-    
-    let w = canvas.width = 602;
-    let h = canvas.height = 855;
+  placeItems(canvas, ctx) {
+    let item = new Image();
+    item.src = this.state.canvas.toDataURL('image/png');
+    let w = item.width / 3;
+    let h = item.height / 3;
 
-    ctx.drawImage(bg, 0, 0, w, h);
-    
-    return ctx;
+    ctx.drawImage( item,
+      0, 0, item.width, item.height,
+      226, 123, w - 2, h - 2
+    );
+
+    ctx.drawImage( item,
+      0, 0, item.width, item.height,
+      225, 123 + 170 - 2, w - 2, h - 1
+    );
+
+    let el = document.getElementById('Palette');
+    el.appendChild(canvas);
   }
 
   openPrint(e) {

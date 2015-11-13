ddec30bc163059cb5d274498ea7e3af57933c0f9
kobabasu <keiji.kobayashi.web@gmail.com>
2015-11-13 13:09:36 +0900

    ソースが複雑になってきたためリファクタリング

--- src/pages/front/Temp.jsx
+++ src/pages/front/Temp.jsx
@@ -4,6 +4,16 @@ import DocumentTitle from 'react-document-title'
 
 import canvasStore from '../../stores/CanvasStore'
 
+let page, pageContext;
+let overlay, overlayContext;
+let items, itemsContext;
+
+let pageWidth = 620;
+let pageHeight = 877;
+
+let templateFile = '../imgs/print_template0';
+
+
 export default class Temp extends React.Component {
 
   constructor(props) {
@@ -40,59 +50,79 @@ export default class Temp extends React.Component {
   }
 
   init() {
-    let id = this.props.params.id;
-    let canvas = document.createElement('canvas');
-    canvas.width = 620;
-    canvas.height = 877;
-    let ctx = canvas.getContext('2d');
-    
-    let item = this.state.px;
-
     let bg = new Image();
-    bg.src = '../imgs/print_template0' + id + '.png';
+    bg.src = templateFile + this.props.params.id + '.png';
     
     let _this = this;
     bg.onload = function() {
-      let items = _this.placeItems(canvas, ctx);
-      ctx.drawImage(items, 0, 0, canvas.width, canvas.height);
-      ctx.globalCompositeOperation = 'darken';
-      ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
+      _this.createPage();
+      _this.createOverlay(bg);
+      _this.createItems();
+
+      pageContext.drawImage(items, 0, 0, pageWidth, pageHeight);
+      pageContext.globalCompositeOperation = 'darken';
+      pageContext.drawImage(overlay, 0, 0, pageWidth, pageHeight);
 
       let el = document.getElementById('Palette');
-      el.appendChild(canvas);
+      el.appendChild(page);
     }
   }
 
-  placeItems() {
+  createPage() {
     let canvas = document.createElement('canvas');
-    canvas.width = 620;
-    canvas.height = 877;
+    canvas.width = pageWidth;
+    canvas.height = pageHeight;
     let ctx = canvas.getContext('2d');
 
-    let item = new Image();
-    item.src = this.state.canvas.toDataURL('image/png');
-    let w = item.width / 3;
-    let h = item.height / 3;
+    page = canvas;
+    pageContext = ctx;
+  }
+
+  createOverlay(bg) {
+    let canvas = document.createElement('canvas');
+    canvas.width = pageWidth;
+    canvas.height = pageHeight;
+    let ctx = canvas.getContext('2d');
+    ctx.drawImage( bg, 0, 0, pageWidth, pageHeight ); 
+    
+    overlay = canvas;
+    overlayContext = ctx;
+  }
+
+  createItems() {
+    let canvas = document.createElement('canvas');
+    let w = canvas.width = pageWidth;
+    let h = canvas.height = pageHeight;
+    let ctx = canvas.getContext('2d');
+
+    let item = this.createItem();
 
     ctx.drawImage( item,
-      0, 0, item.width, item.height,
+      0, 0, w, h,
       225, 122, w, h
     );
 
-    ctx.drawImage( item,
-      0, 0, item.width, item.height,
-      225, 122 + 169, w, h
-    );
-
-    ctx.drawImage( item,
-      0, 0, item.width, item.height,
-      225, 122 + 169, w, h
-    );
-
-
-    let items = new Image();
+    items = new Image();
     items.src = canvas.toDataURL('image/png');
-    return items;
+  }
+
+  createItem() {
+    let canvas = document.createElement('canvas');
+    let ctx = canvas.getContext('2d');
+    let source = this.state.canvas;
+    let w = canvas.width = source.width / 3;
+    let h = canvas.height = source.height / 3;
+    console.log(w, source.width);
+
+    ctx.translate( w / 2, h / 2 );
+    ctx.rotate( 180/180*Math.PI );
+    ctx.translate( -(w / 2), -(h / 2) );
+    ctx.drawImage( source, 0, 0, w, h );
+
+    let item = new Image();
+    item.src = canvas.toDataURL('image/png');
+
+    return item;
   }
 
   openPrint(e) {

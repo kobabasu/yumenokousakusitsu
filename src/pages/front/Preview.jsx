84d2583e0f2ed9f8fbd349a46e2106842d508351
kobabasu <keiji.kobayashi.web@gmail.com>
2016-02-15 18:59:10 +0900

    css3dを追加

--- src/pages/front/Preview.jsx
+++ src/pages/front/Preview.jsx
@@ -12,6 +12,7 @@ export default class Preview extends React.Component {
   }
 
   componentWillMount() {
+    this.setState( canvasStore.read(), this.init );
   }
 
   render() {
@@ -28,6 +29,33 @@ export default class Preview extends React.Component {
         <p className="pdnTop10 mgnBtm20 f20">
           マウスで動かして全体を見ることができます。
         </p>
+
+        <section id="stage" className="stage">
+          <div className="view">
+            <div className="panel p1">
+              <img
+                src="/drawing/upload/20160215-012544.png"
+                alt="canvas"
+                width="200"
+                height="200"
+                />
+            </div>
+
+            <div className="panel p2">
+              <img
+                src="/drawing/upload/20160215-012544.png"
+                alt="canvas"
+                width="200"
+                height="200"
+                />
+            </div>
+
+            <div className="panel p3">3</div>
+            <div className="panel p4">4</div>
+            <div className="panel p5">5</div>
+            <div className="panel p6">6</div>
+          </div>
+        </section>
 
         <nav className="alignCenter">
           <div className="drawPreview01">

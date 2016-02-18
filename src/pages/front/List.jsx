5c7f139a8eda28a146d0fa711b9dfcb9ed5b6d71
kobabasu <keiji.kobayashi.web@gmail.com>
2016-02-18 14:50:52 +0900

    userモデルの読込。ボタンの追加

--- src/pages/front/List.jsx
+++ src/pages/front/List.jsx
@@ -4,6 +4,9 @@ import DocumentTitle from 'react-document-title'
 
 import canvasStore from '../../stores/CanvasStore'
 
+import userActions from '../../actions/UserActions'
+import userStore from '../../stores/UserStore'
+
 export default class List extends React.Component {
 
   constructor(props) {
@@ -11,30 +14,58 @@ export default class List extends React.Component {
   }
 
   componentWillMount() {
-    let id = this.props.params.id;
+    userStore.subscribe(this.update.bind(this));
+    userActions.read();
   }
 
   render() {
-    let tmp = [
-      {
-        'path': '/drawing/upload/20160215-012544.png',
-        'name': 'ニックちゃん'
-      },
-      {
-        'path': '/drawing/upload/20160215-012544.png',
-        'name': 'ニックちゃん2'
-      }
-    ];
+    if (!this.state) return false
 
-    let items = Object.keys(tmp).map((i) => {
+    let items = Object.keys(this.state.pages).map((i) => {
       return (
         <ListItems
           key={i}
-          path={tmp[i].path}
-          name={tmp[i].name}
+          path={'/drawing/upload/' + this.state.pages[i].path}
+          name={this.state.pages[i].name}
           />
       );
     });
+
+    let disableBack = 1;
+    let disableNext = 1;
+    let pageBack = null;
+    let pageNext = null;
+
+    let id = parseInt(this.props.params.id);
+    switch (id) {
+      case 1:
+        disableBack = 1;
+        disableNext = 0;
+        pageBack = null;
+        pageNext = 2;
+        break;
+
+      case 2:
+        disableBack = 0;
+        disableNext = 0;
+        pageBack = 1;
+        pageNext = 3;
+        break;
+
+      case 3:
+        disableBack = 0;
+        disableNext = 1;
+        pageBack = 2;
+        pageNext = null;
+        break;
+
+      default:
+        // no OP
+        break;
+    }
+
+    let disable = 0;
+    let page = this.props.params.id - 1;
 
     return (
       <div className="drawCont fbox alignCenter" id="List">
@@ -55,31 +86,15 @@ export default class List extends React.Component {
         </div>
 
         <nav className="alignCenter">
-          <div className="drawList01">
-            <a
-              href="/drawing/"
-              >
-              <img
-                src="../imgs/clear.gif"
-                alt="前へ戻る"
-                width="150"
-                height="50"
-                />
-            </a>
-          </div>
+          <ListBack
+            disable={disableBack}
+            page={pageBack}
+            />
 
-          <div className="drawList02">
-            <a
-              href="/drawing/"
-              >
-              <img
-                src="../imgs/clear.gif"
-                alt="次へ進む"
-                width="150"
-                height="50"
-                />
-            </a>
-          </div>
+          <ListNext
+            disable={disableNext}
+            page={pageNext}
+            />
 
           <div className="drawList03">
             <a
@@ -96,6 +111,13 @@ export default class List extends React.Component {
         </nav>
       </div>
     );
+  }
+
+  update() {
+    this.setState({ pages: userStore.read() });
+  }
+
+  getPages() {
   }
 }
 
@@ -119,6 +141,56 @@ class ListItems extends React.Component {
         />
         <p>{this.props.name}</p>
       </div>
+    );
+  }
+}
+
+class ListBack extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    if (this.props.disable == 1) return false
+
+    return (
+      <div className="drawList01">
+        <a
+          href="/drawing/"
+          >
+          <img
+            src="../imgs/clear.gif"
+            alt="前へ戻る"
+            width="150"
+            height="50"
+            />
+        </a>
+      </div>
+    );
+  }
+}
+
+class ListNext extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    if (this.props.disable == 1) return false
+
+    return (
+        <div className="drawList02">
+          <a
+            href="/drawing/"
+            >
+            <img
+              src="../imgs/clear.gif"
+              alt="次へ進む"
+              width="150"
+              height="50"
+              />
+          </a>
+        </div>
     );
   }
 }
